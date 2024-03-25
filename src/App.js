f9d2eb35d3ae37f0888dd339eef2cd139a0752eb
Siddiqui0013 <asifm8702@gmail.com>
2024-03-25 21:41:28 +0500

    Created Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";  
+import ForgotPass from "./pages/ForgotPass";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+
+
 import './App.css';
 
 function App() {
   return (
     <>
-     <p className="text-3xl font-bold underline">React App</p>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="Profile" element={<Profile/>}></Route>
+        <Route path="ForgotPass" element={<ForgotPass/>}></Route>
+        <Route path="Signin" element={<Signin/>}></Route>
+        <Route path="Signup" element={<Signup/>}></Route>
+        <Route path="/" element={<Home/>}></Route>
+
+      </Routes>
+    </Router>
+     {/* <p className="text-3xl font-bold underline">React App</p> */}
     </>
   );
 }
