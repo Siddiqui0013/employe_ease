09ed668815199c1f9b2fd3e88e59c83f08aa1545
siddiqui0013 <asifm8702@gmail.com>
2024-04-26 16:04:31 +0500

    ceaser to signin

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -2,6 +2,69 @@ import React from 'react'
 
 export default function Signin() {
   return (
-    <div>Signin</div>
+
+    <div className="container">
+      <div className="signin">
+
+        <h1>Sign-in</h1>
+        
+        <div className="text">
+          <input type="text" placeholder="Enter your Plain Text" />
+        </div>
+
+        <div className="key">
+          <input type="number" min="-26" max="26" placeholder="Enter Key (-26 to +26)" />
+        </div>
+
+        <div className="button">
+          <button className="btn">Encrypt</button>
+        </div>
+
+        <div className="encrypted">
+          <div className="solutionText">
+            <h3>The Encrypted text is :</h3>
+          </div>
+          <div className="solInput">
+            <input type="text" readOnly />
+          </div>
+        </div>
+
+        <div className="toDecrypt">
+          <p>Want to Decrypt a Cipher ?</p>
+          <button className="decryptBtn">Decrypt</button>
+        </div>
+      
+      </div>
+
+      <div className="decrypt">
+
+        <h1>Decrypt Cipher Text</h1>
+        
+        <div className="text">
+          <input type="text" placeholder="Enter your Cipher Text" />
+        </div>
+
+        <div className="key">
+          <input type="text" placeholder="Enter Key (-26 to +26)" />
+        </div>
+
+        <div className="button">
+          <button className="btn">Decrypt</button>
+        </div>
+
+        <div className="encrypted">
+          <div className="solutionText">
+            <h3>The Encrypted text is :</h3>
+          </div>
+          <input type="text" readOnly />
+        </div>
+
+        <div className="toDecrypt">
+          <p>Want to Encrypt a Plain text?</p>
+          <button className="decryptBtn">Encrypt</button>
+        </div>
+      </div> 
+    </div>
+
   )
 }
